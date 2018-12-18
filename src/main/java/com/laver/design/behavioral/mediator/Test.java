e2a69f457514fa8d91dac900167edfe23845752a
laver <447205752@qq.com>
2018-12-18 21:17:24 +0800

    mediator

---
+++ src/main/java/com/laver/design/behavioral/mediator/Test.java
@@ -0,0 +1,9 @@
+package com.laver.design.behavioral.mediator;
+
+public class Test {
+    public static void main(String[] args) {
+        User laver = new User("laver");
+        laver.sendMessage("Hello World");
+
+    }
+}
